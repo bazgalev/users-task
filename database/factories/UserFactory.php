47d32aa0d2fa0c99a9c17f1e983fd97fcc07d35b
pazgalev <m.pazgalev@oy2b.ru>
2020-01-13 23:48:35 +0300

    Create factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,9 +1,10 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\City;
 use App\User;
 use Faker\Generator as Faker;
-use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,11 +18,11 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $name = explode(' ', $faker->name);
     return [
-        'name' => $faker->name,
+        'first_name' => $name[0],
+        'last_name' => $name[1],
+        'patronymic' => $name[2],
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
     ];
 });
