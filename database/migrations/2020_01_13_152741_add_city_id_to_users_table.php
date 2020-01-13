e906b829684e679d6dade79f3c2381c06c26ae81
pazgalev <m.pazgalev@oyster-telecom.ru>
2020-01-13 18:37:29 +0300

    Added migrations

---
+++ database/migrations/2020_01_13_152741_add_city_id_to_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCityIdToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('city_id')->after('patronymic');
+            $table->foreign('city_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::table('users', function (Blueprint $table) {
+//            $table->dropForeign('cities_city')
+        });
+    }
+}
